6bfedf543fccae0e0bb06496e9b1abcd8dd53c3a
josephjlyons <josephjlyons90@gmail.com>
2022-11-09 15:02:09 -0500

    create section for certs, place in grid

---
+++ src/Components/Certification/Certifications.js
@@ -0,0 +1,12 @@
+import '../../sass/_certifications.scss';
+
+const Certifications = () => {
+    return (
+        <div className='certifications'>
+
+            <p className='header__h1'>Certifications</p>
+        </div>
+    )
+}
+
+export default Certifications;
