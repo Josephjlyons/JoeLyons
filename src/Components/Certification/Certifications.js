124da7b9c4f2de78b505578c7133267171ff3ce1
josephjlyons <josephjlyons90@gmail.com>
2022-11-15 10:19:33 -0500

    add hover to certs

--- src/Components/Certification/Certifications.js
+++ src/Components/Certification/Certifications.js
@@ -15,15 +15,15 @@ const Certifications = () => {
             
                 <figure className='certifications__item--1'><img src={unhCert} alt='advanced css sass cert' className='gal' />
                 <p className='certifications__text'>UNH Coding and Programming</p></figure>
-                <figure className='certifications__item--2'><img src={advancedCSS} alt='advanced css sass cert' className='gal' />
+                <figure className='certifications__item'><img src={advancedCSS} alt='advanced css sass cert' className='gal' />
                 <p className='certifications__text'>Advanced CSS and Sass</p></figure>
-                <figure className='certifications__item--3'><img src={webdevCert} alt='advanced css sass cert' className='gal' />
+                <figure className='certifications__item'><img src={webdevCert} alt='advanced css sass cert' className='gal' />
                 <p className='certifications__text'>Web Dev Bootcamp 2022</p></figure>
-                <figure className='certifications__item--4'><img src={reactCert} alt='advanced css sass cert' className='gal' />
+                <figure className='certifications__item'><img src={reactCert} alt='advanced css sass cert' className='gal' />
                 <p className='certifications__text'>React - The Complete Guide</p></figure>
-                <figure className='certifications__item--5'><img src={algodataCert} alt='advanced css sass cert' className='gal' />
+                <figure className='certifications__item'><img src={algodataCert} alt='advanced css sass cert' className='gal' />
                 <p className='certifications__text'>Algorithms + Data Structures</p></figure>
-                <figure className='certifications__item--5'><img src={typescriptCert} alt='advanced css sass cert' className='gal' />
+                <figure className='certifications__item'><img src={typescriptCert} alt='advanced css sass cert' className='gal' />
                 <p className='certifications__text'>Understanding Typescript</p></figure>
             </div>
 
