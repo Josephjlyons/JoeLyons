3688b9e1ba5620253dc7991f0dc005522644d613
josephjlyons <josephjlyons90@gmail.com>
2022-11-22 13:33:08 -0500

    start build of works section | img & grid layout |

--- src/Components/Portfolio/Portfolio.js
+++ src/Components/Portfolio/Portfolio.js
@@ -1,10 +1,24 @@
-
 import '../../sass/_portfolio.scss';
+import yelpCamp from '../../img/yelp-camp-img.png';
+import draughtPicker from '../../img/draught-picker.png';
+import dnd from '../../img/dnd-homepage-Img.png';
 
 const Portfolio = () => {
     return(
 
-        <div className='portfolio'>Portfolio</div>
+        <div id='portfolio' className='portfolio'>
+        
+        {/* <h2 className='portfolio__heading'> Portfolio</h2> */}
+
+
+        <figure className='portfolio__item portfolio__item--1'><img src={yelpCamp} alt='yelpcamp homepage' className='worksImg' />
+       
+        </figure>
+        <figure className='portfolio__item portfolio__item--2'><img src={draughtPicker} alt='draught homepage' className='worksImg' />
+        </figure>
+        <figure className='portfolio__item portfolio__item--3'><img src={dnd} alt='dnd homepage' className='worksImg' />
+        </figure>
+        </div>
     )
 }
 
