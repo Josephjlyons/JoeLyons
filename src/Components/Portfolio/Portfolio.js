bda7c94a38e5ec0474a9267612c180a2f76ba970
josephjlyons <josephjlyons90@gmail.com>
2022-12-06 15:45:53 -0500

    clean up files of uneeded/unused code

--- src/Components/Portfolio/Portfolio.js
+++ src/Components/Portfolio/Portfolio.js
@@ -1,7 +1,5 @@
 import '../../sass/_portfolio.scss';
-// import yelpCamp from '../../img/yelp-camp-img.png';
-// import draughtPicker from '../../img/draught-picker.png';
-// import dnd from '../../img/dnd-homepage-Img.png';
+
 
 const Portfolio = () => {
     return (
@@ -9,23 +7,23 @@ const Portfolio = () => {
         <div id='portfolio' className='portfolio'>
 
             <figure className='portfolio__item portfolio__item--1'>
-                <p className='portfolio__header--yelp'>YelpCamp</p>
+                <p className='portfolio__header'>YelpCamp</p>
                 <div className='portfolio__item--hidden'>hidden text test
                 <button>test</button> </div>
                 
-                {/* <img src={yelpCamp} alt='yelpcamp homepage' className='worksImg' /> */}
+           
             </figure>
 
             <figure className='portfolio__item portfolio__item--2'>
-                <p className='portfolio__header--draught'>Draught Picker</p>
+                <p className='portfolio__header'>Draught Picker</p>
                 <div className='portfolio__item--hidden'>hidden text test </div>
-                {/* <img src={draughtPicker} alt='draught homepage' className='worksImg' /> */}
+         
             </figure>
 
             <figure className='portfolio__item portfolio__item--3'>
-                <p className='portfolio__header--dnd'>DnD Creator</p>
+                <p className='portfolio__header'>DnD Creator</p>
                 <div className='portfolio__item--hidden'>hidden text test </div>
-                {/* <img src={dnd} alt='dnd homepage' className='worksImg' /> */}
+ 
             </figure>
 
         </div>
