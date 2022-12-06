d3f0ee36ceb560284797c5ca8fea2c4025ab5f0f
josephjlyons <josephjlyons90@gmail.com>
2022-12-06 12:57:28 -0500

    hover animation moving with heading

--- src/Components/Portfolio/Portfolio.js
+++ src/Components/Portfolio/Portfolio.js
@@ -4,20 +4,22 @@ import draughtPicker from '../../img/draught-picker.png';
 import dnd from '../../img/dnd-homepage-Img.png';
 
 const Portfolio = () => {
-    return(
+    return (
 
         <div id='portfolio' className='portfolio'>
-        
-        {/* <h2 className='portfolio__heading'> Portfolio</h2> */}
 
+                <figure className='portfolio__item portfolio__item--1'><p className='portfolio__header--yelp'>YelpCamp</p>
+                <img src={yelpCamp} alt='yelpcamp homepage' className='worksImg' />
+                </figure>
 
-        <figure className='portfolio__item portfolio__item--1'><img src={yelpCamp} alt='yelpcamp homepage' className='worksImg' />
-       
-        </figure>
-        <figure className='portfolio__item portfolio__item--2'><img src={draughtPicker} alt='draught homepage' className='worksImg' />
-        </figure>
-        <figure className='portfolio__item portfolio__item--3'><img src={dnd} alt='dnd homepage' className='worksImg' />
-        </figure>
+                <figure className='portfolio__item portfolio__item--2'>  <p className='portfolio__header--draught'>Draught Picker</p>
+                <img src={draughtPicker} alt='draught homepage' className='worksImg' />
+                </figure>
+
+                <figure className='portfolio__item portfolio__item--3'><p className='portfolio__header--dnd'>DnD Creator</p>
+                <img src={dnd} alt='dnd homepage' className='worksImg' />
+                </figure>
+
         </div>
     )
 }
