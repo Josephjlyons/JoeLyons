b57aad8afd3fe7d11d7a86dc4e9a56993582fb59
josephjlyons <josephjlyons90@gmail.com>
2022-12-06 15:32:39 -0500

    start creating text on hover and img opacity change

--- src/Components/Portfolio/Portfolio.js
+++ src/Components/Portfolio/Portfolio.js
@@ -1,24 +1,32 @@
 import '../../sass/_portfolio.scss';
-import yelpCamp from '../../img/yelp-camp-img.png';
-import draughtPicker from '../../img/draught-picker.png';
-import dnd from '../../img/dnd-homepage-Img.png';
+// import yelpCamp from '../../img/yelp-camp-img.png';
+// import draughtPicker from '../../img/draught-picker.png';
+// import dnd from '../../img/dnd-homepage-Img.png';
 
 const Portfolio = () => {
     return (
 
         <div id='portfolio' className='portfolio'>
 
-                <figure className='portfolio__item portfolio__item--1'><p className='portfolio__header--yelp'>YelpCamp</p>
-                <img src={yelpCamp} alt='yelpcamp homepage' className='worksImg' />
-                </figure>
+            <figure className='portfolio__item portfolio__item--1'>
+                <p className='portfolio__header--yelp'>YelpCamp</p>
+                <div className='portfolio__item--hidden'>hidden text test
+                <button>test</button> </div>
+                
+                {/* <img src={yelpCamp} alt='yelpcamp homepage' className='worksImg' /> */}
+            </figure>
 
-                <figure className='portfolio__item portfolio__item--2'>  <p className='portfolio__header--draught'>Draught Picker</p>
-                <img src={draughtPicker} alt='draught homepage' className='worksImg' />
-                </figure>
+            <figure className='portfolio__item portfolio__item--2'>
+                <p className='portfolio__header--draught'>Draught Picker</p>
+                <div className='portfolio__item--hidden'>hidden text test </div>
+                {/* <img src={draughtPicker} alt='draught homepage' className='worksImg' /> */}
+            </figure>
 
-                <figure className='portfolio__item portfolio__item--3'><p className='portfolio__header--dnd'>DnD Creator</p>
-                <img src={dnd} alt='dnd homepage' className='worksImg' />
-                </figure>
+            <figure className='portfolio__item portfolio__item--3'>
+                <p className='portfolio__header--dnd'>DnD Creator</p>
+                <div className='portfolio__item--hidden'>hidden text test </div>
+                {/* <img src={dnd} alt='dnd homepage' className='worksImg' /> */}
+            </figure>
 
         </div>
     )
