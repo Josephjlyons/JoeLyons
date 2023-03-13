51ef403ab5f7eb26a97060f1e9b78b2166e943fe
josephjlyons <josephjlyons90@gmail.com>
2023-03-13 12:40:20 -0400

    remove about me section (contact) for now

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 // import logo from './logo.svg';
 // import './App.css';
 import './css/style.css';
-import Sidebar from './Components/Sidebar/Sidebar';
+// import Sidebar from './Components/Sidebar/Sidebar';
 import Header from './Components/Header/Header';
 import About from './Components/About/About';
 import Portfolio from './Components/Portfolio/Portfolio';
-import Contact from './Components/Contact/Contact';
+// import Contact from './Components/Contact/Contact';
 import Footer from './Components/Footer/Footer';
+import Certifications from './Components/Certification/Certifications';
+import './sass/_base.scss';
 
 function App() {
   return (
     <div className="App">
       <div className='container'>
-        <Sidebar />
+        {/* <Sidebar /> */}
         <Header />
         <About />
+        <Certifications />
         <Portfolio />
-        <Contact />
+        {/* <Contact /> */}
         <Footer />
       </div>
 
