0d7aa66a520cf7b4110d85e1909f3665e1308502
josephjlyons <josephjlyons90@gmail.com>
2023-03-17 12:03:53 -0400

    start adjusting about container grid to be more responsive

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,48 +1,50 @@
 import React from 'react';
 import '../../sass/_about.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faJsSquare, faReact, faNodeJs, faSass, faHtml5} from '@fortawesome/free-brands-svg-icons';
-import {faDatabase} from '@fortawesome/free-solid-svg-icons';
+import { faJsSquare, faReact, faNodeJs, faSass, faHtml5 } from '@fortawesome/free-brands-svg-icons';
+import { faDatabase } from '@fortawesome/free-solid-svg-icons';
 
 
 const About = () => {
     return (
-        <div id='about' className='about'>
+        // <div id='about' className='about'>
+<div className='features'>
 
-            <div className='feature'>
+
+            <div className='feature feature--1'>
                 <FontAwesomeIcon className='feature__icon' icon={faJsSquare} />
                 <h4 className='feature__heading'>Javascript</h4>
                 <p className='feature__text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis laborum cupiditate amet? adipisicing elit.</p>
             </div>
-            <div className='feature'>
-        
+
+            <div className='feature feature--2'>
                 <FontAwesomeIcon className='feature__icon' icon={faReact} />
                 <h4 className='feature__heading'>React</h4>
                 <p className='feature__text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis laborum cupiditate amet?</p>
             </div>
-            <div className='feature'>
-            <FontAwesomeIcon className='feature__icon' icon={faNodeJs} />
+            <div className='feature feature--3'>
+                <FontAwesomeIcon className='feature__icon' icon={faNodeJs} />
                 <h4 className='feature__heading'>Nodejs</h4>
                 <p className='feature__text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis laborum cupiditate amet?</p>
             </div>
-            <div className='feature'>
+            <div className='feature feature--4'>
                 <FontAwesomeIcon className='feature__icon' icon={faSass} />
                 <h4 className='feature__heading'>Css/Sass</h4>
                 <p className='feature__text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis laborum cupiditate amet?</p>
             </div>
-            <div className='feature'>
-            <FontAwesomeIcon className='feature__icon' icon={faDatabase} />
+            <div className='feature feature--5'>
+                <FontAwesomeIcon className='feature__icon' icon={faDatabase} />
                 <h4 className='feature__heading'>MongoDB</h4>
                 <p className='feature__text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis laborum cupiditate amet?</p>
             </div>
-            <div className='feature'>
+            <div className='feature feature--6'>
                 <FontAwesomeIcon className='feature__icon' icon={faHtml5} />
                 <h4 className='feature__heading'>HTML</h4>
                 <p className='feature__text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis laborum cupiditate amet?</p>
             </div>
 
-     
-        </div>
+            </div>
+        // </div>
     )
 }
 
