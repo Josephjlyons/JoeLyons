b65ed520ac06f6778cbcd0039528f88ffc8307a3
josephjlyons <josephjlyons90@gmail.com>
2023-04-12 12:30:19 -0400

    remove unused components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import './css/style.css';
-// import Sidebar from './Components/Sidebar/Sidebar';
 import Header from './Components/Header/Header';
 import About from './Components/About/About';
 import Portfolio from './Components/Portfolio/Portfolio';
-// import Contact from './Components/Contact/Contact';
 import Footer from './Components/Footer/Footer';
 import Certifications from './Components/Certification/Certifications';
 import './sass/_base.scss';
@@ -12,12 +10,10 @@ function App() {
   return (
     <div className="App">
       <div className='container'>
-        {/* <Sidebar /> */}
         <Header />
         <About />
         <Certifications />
         <Portfolio />
-        {/* <Contact /> */}
         <Footer />
       </div>
 
