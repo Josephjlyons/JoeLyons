622d111017aec68b4c17e49d306e4e2c16343526
josephjlyons <josephjlyons90@gmail.com>
2023-04-14 10:17:55 -0400

    revert away from carousel to prevent page change on slide move

--- src/Components/Certification/Certifications.js
+++ src/Components/Certification/Certifications.js
@@ -1,73 +1,88 @@
 import '../../sass/_certifications.scss';
-import advancedCSS from '../../img/advanced-css-sass.webp';
-import unhCert from '../../img/unh-coding-cert.webp';
-import webdevCert from '../../img/webdevboot-cert.webp';
-import reactCert from '../../img/reactCert.webp';
-import typescriptCert from '../../img/typescriptCert.webp';
-import algodataCert from '../../img/algodataCert.webp';
-import Slider from 'react-slick';
-import '../../sass/_slick-theme.scss';
-import '../../sass/_slick.scss';
+// import advancedCSS from '../../img/advanced-css-sass.webp';
+// import unhCert from '../../img/unh-coding-cert.webp';
+// import webdevCert from '../../img/webdevboot-cert.webp';
+// import reactCert from '../../img/reactCert.webp';
+// import typescriptCert from '../../img/typescriptCert.webp';
+// import algodataCert from '../../img/algodataCert.webp';
+
+
+import UNNNH from '../../img/unh-coding-cert.png';
+import Adavanced from '../../img/advanced-css-sass.jpg';
+import algo from '../../img/algodataCert.jpg';
+import react from '../../img/reactCert.jpg';
+import webdev from '../../img/webdevboot-certSM.jpg';
+import typescript from '../../img/typescriptCert.jpg';
+import FadeInSection from '../../FadeInSections';
+// import Slider from 'react-slick';
+// import '../../sass/_slick-theme.scss';
+// import '../../sass/_slick.scss';
+// import FadeInSection from '../../FadeInSections';
 
 const Certifications = () => {
-    const settings = {
+    // const settings = {
 
-        dots: false,
-        arrows: true,
-        adaptiveHeight: true,
-        fade:true,
-        // autoplay: true,
-        // centerMode: true,
-        autoplaySpeed: 3000,
-        pauseOnHover: true,
-        lazyLoad: true,
-        infinite: true,
-        speed: 5,
+    //     dots: false,
+    //     arrows: true,
+    //     adaptiveHeight: true,
+    //     fade:true,
+    //     // autoplay: true,
+    //     // centerMode: true,
+    //     autoplaySpeed: 3000,
+    //     pauseOnHover: true,
+    //     lazyLoad: true,
+    //     infinite: true,
+    //     speed: 5,
 
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        responsive: [
-            {
-                breakpoint: 740,
-                settings: {
-                    slidesToShow: 1,
+    //     slidesToShow: 1,
+    //     slidesToScroll: 1,
+    //     responsive: [
+    //         {
+    //             breakpoint: 740,
+    //             settings: {
+    //                 slidesToShow: 1,
 
-                }
-            },
+    //             }
+    //         },
 
-        ]
+    //     ]
 
 
-    };
+    // };
     return (
 
         <div className='certifications'>
-            <Slider className='certifications__slick-slider' {...settings}>
+     
+            {/* <Slider className='certifications__slick-slider' {...settings}> */}
 
-                <figure className='certifications__item certifications__item--1'><img src={unhCert}
+
+            
+
+                <figure className='certifications__item certifications__item--1'><img src={UNNNH}
                     alt='Unh Certification' />
                 </figure>
-
-                <figure className='certifications__item certifications__item--2'><img src={webdevCert}
+           
+                <figure className='certifications__item certifications__item--2'><img src={webdev}
                     alt='Web Dev Certification' />
                 </figure>
 
-                <figure className='certifications__item certifications__item--3'><img src={reactCert}
+                <figure className='certifications__item certifications__item--3'><img src={react}
                     alt='React Certification' />
                 </figure>
 
-                <figure className='certifications__item certifications__item--4'><img src={advancedCSS}
+                <figure className='certifications__item certifications__item--4'><img src={Adavanced}
                     alt='Advanced Css and Sass Certification' />
                 </figure>
 
-                <figure className='certifications__item certifications__item--5'><img src={typescriptCert}
+                <figure className='certifications__item certifications__item--5'><img src={typescript}
                     alt='Typescript Certification' />
                 </figure>
 
-                <figure className='certifications__item certifications__item--6'><img src={algodataCert}
+                <figure className='certifications__item certifications__item--6'><img src={algo}
                     alt='Algorithms Certification' />
                 </figure>
-            </Slider>
+            {/* </Slider> */}
+           
         </div>
 
 
