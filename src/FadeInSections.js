50bbfb3efcaa8551c535ccc6903affb9dd9944c1
josephjlyons <josephjlyons90@gmail.com>
2023-04-14 10:35:07 -0400

    fade in on scroll logic

---
+++ src/FadeInSections.js
@@ -0,0 +1,34 @@
+import React, { useState, useRef, useEffect } from "react";
+import '../src/sass/_fadein.scss';
+
+export default function FadeInSection(props) {
+  const [isVisible, setVisible] = useState(false);
+
+  const domRef = useRef();
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(entries => {
+  
+      if (entries[0].isIntersecting) {
+       
+        setVisible(true);
+
+
+        observer.unobserve(domRef.current);
+      }
+    });
+
+    observer.observe(domRef.current);
+
+
+  }, []);
+
+  return (
+    <div
+      ref={domRef}
+      className={`fade-in-section ${isVisible ? "is-visible" : ""}`}
+    >
+      {props.children}
+    </div>
+  );
+}
