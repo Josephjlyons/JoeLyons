6bfb22935b9dcd3e008524a2bbe01f3dbe3a3b47
josephjlyons <josephjlyons90@gmail.com>
2023-04-18 10:28:41 -0400

    add in correct imports for file changes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
+import React from 'react';
 import './css/style.css';
+import './sass/_base.scss';
+
 import Header from './Components/Header/Header';
 import About from './Components/About/About';
 import Portfolio from './Components/Portfolio/Portfolio';
 import Footer from './Components/Footer/Footer';
 import Certifications from './Components/Certification/Certifications';
-import './sass/_base.scss';
+import Skills from './Components/About/Skills';
+
 
 function App() {
   return (
     <div className="App">
       <div className='container'>
-        <Header />
+        <Header/>
         <About />
+        <Skills />
         <Certifications />
         <Portfolio />
         <Footer />
