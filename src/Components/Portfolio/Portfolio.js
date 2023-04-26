9f613f73602ab5d288cfb60bd9604317d552d45d
josephjlyons <josephjlyons90@gmail.com>
2023-04-26 12:36:09 -0400

    base layout for portfolio section

--- src/Components/Portfolio/Portfolio.js
+++ src/Components/Portfolio/Portfolio.js
@@ -6,13 +6,14 @@ import yelpCampMain from '../../img/Screenshot 2022-11-22 at 12-57-42 YelpCamp.p
 import draughtPickerMain from '../../img/Screenshot 2022-11-22 at 12-48-01 The Draught Picker.png';
 import dndCreator from '../../img/dnd-homepage-Img.webp.png';
 
+
 const Portfolio = () => {
     return (
 
         <div id='portfolio' className='portfolio'>
 
             <figure className='portfolio__item portfolio__item--1'>
-                <p className='portfolio__header'>YelpCamp</p>
+                {/* <p className='portfolio__header'>YelpCamp</p> */}
                 <img className='portfolio__item portfolio__item--yelp' src={yelpCampMain} alt='yelp-camp-main'></img>
                 <div className='portfolio__item--hidden'>
                 <ul className='portfolio__item--list1'>
@@ -23,41 +24,40 @@ const Portfolio = () => {
                         <li>Easy to use interface</li>
                     </ul>
 
-                    <a href="https://github.com/Josephjlyons/YelpCamp"><FontAwesomeIcon className="header__icons" icon={faGithub} /></a>
+                    <a href="https://github.com/Josephjlyons/YelpCamp" aria-label='github'><FontAwesomeIcon className="portfolio__icons" icon={faGithub} /></a>
                 </div>
-
             </figure>
 
             <figure className='portfolio__item portfolio__item--2'>
-                <p className='portfolio__header'>Draught Picker</p>
-                <img className='portfolio__item portfolio__item--draught' src={draughtPickerMain} alt='yelp-camp-main'></img>
+                {/* <p className='portfolio__header'>Draught Picker</p> */}
+                <img className='portfolio__item portfolio__item--draught' src={draughtPickerMain} alt='draught-main'></img>
                 <div className='portfolio__item--hidden'>
                     <ul className='portfolio__item--list2'>
                         <li>OpenBreweryDB API</li>
                         <li>Search by zip codes</li>
                         <li>Favorite and save breweries</li>
-                        <li>View information of breweries</li>
+                        <li>View brewery info</li>
                         <li>Map location of brewery</li>
                     </ul>
-                    <a href="https://github.com/Josephjlyons/DraughtPicker"><FontAwesomeIcon className="header__icons" icon={faGithub} /></a>
-                    <a href='https://josephjlyons.github.io/DraughtPicker/'><FontAwesomeIcon className="header__icons" icon={faGlobe} /></a>
+                    <a href="https://github.com/Josephjlyons/DraughtPicker"aria-label='github'><FontAwesomeIcon className="portfolio__icons" icon={faGithub} /></a>
+                    <a href='https://josephjlyons.github.io/DraughtPicker/'aria-label='website'><FontAwesomeIcon className="portfolio__icons" icon={faGlobe} /></a>
                 </div>
 
             </figure>
 
             <figure className='portfolio__item portfolio__item--3'>
-                <p className='portfolio__header'>DnD Creator</p>
-                <img className='portfolio__item portfolio__item--dnd' src={dndCreator} alt='yelp-camp-main'></img>
+                {/* <p className='portfolio__header'>DnD Creator</p> */}
+                <img className='portfolio__item portfolio__item--dnd' src={dndCreator} alt='dnd-main'></img>
                 <div className='portfolio__item--hidden'>
 
                 <ul className='portfolio__item--list1'>
                         <li>Secure Bcrypt login</li>
                         <li>Fast character creation</li>
-                        <li>generated stats from DND API </li>
+                        <li>generated from DND API </li>
                         <li>view multiple characters</li>
                         <li>Easily swap characters</li>
                     </ul>
-                    <a href="https://github.com/tjvig94/dnd-exp"><FontAwesomeIcon className="header__icons" icon={faGithub} /></a>
+                    <a href="https://github.com/tjvig94/dnd-exp" aria-label='github'><FontAwesomeIcon className="portfolio__icons" icon={faGithub} /></a>
                 </div>
 
             </figure>
@@ -66,4 +66,5 @@ const Portfolio = () => {
     )
 }
 
+   
 export default Portfolio;
