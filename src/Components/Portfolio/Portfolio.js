006f7bd88afe238e7e6542edfa9eda579b7567cb
josephjlyons <josephjlyons90@gmail.com>
2023-04-26 11:37:21 -0400

    start rework of portfolio section -> mobile friendly

--- src/Components/Portfolio/Portfolio.js
+++ src/Components/Portfolio/Portfolio.js
@@ -2,6 +2,9 @@ import '../../sass/_portfolio.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faGithub} from '@fortawesome/free-brands-svg-icons';
 import {faGlobe} from '@fortawesome/free-solid-svg-icons';
+import yelpCampMain from '../../img/Screenshot 2022-11-22 at 12-57-42 YelpCamp.png';
+import draughtPickerMain from '../../img/Screenshot 2022-11-22 at 12-48-01 The Draught Picker.png';
+import dndCreator from '../../img/dnd-homepage-Img.webp.png';
 
 const Portfolio = () => {
     return (
@@ -10,8 +13,9 @@ const Portfolio = () => {
 
             <figure className='portfolio__item portfolio__item--1'>
                 <p className='portfolio__header'>YelpCamp</p>
+                <img className='portfolio__item portfolio__item--yelp' src={yelpCampMain} alt='yelp-camp-main'></img>
                 <div className='portfolio__item--hidden'>
-                <ul className='portfolio__list'>
+                <ul className='portfolio__item--list1'>
                         <li>Secure passport login</li>
                         <li>Interactive Map</li>
                         <li>image uploading</li>
@@ -26,9 +30,9 @@ const Portfolio = () => {
 
             <figure className='portfolio__item portfolio__item--2'>
                 <p className='portfolio__header'>Draught Picker</p>
+                <img className='portfolio__item portfolio__item--draught' src={draughtPickerMain} alt='yelp-camp-main'></img>
                 <div className='portfolio__item--hidden'>
-
-                    <ul className='portfolio__list'>
+                    <ul className='portfolio__item--list2'>
                         <li>OpenBreweryDB API</li>
                         <li>Search by zip codes</li>
                         <li>Favorite and save breweries</li>
@@ -43,9 +47,10 @@ const Portfolio = () => {
 
             <figure className='portfolio__item portfolio__item--3'>
                 <p className='portfolio__header'>DnD Creator</p>
+                <img className='portfolio__item portfolio__item--dnd' src={dndCreator} alt='yelp-camp-main'></img>
                 <div className='portfolio__item--hidden'>
 
-                <ul className='portfolio__list'>
+                <ul className='portfolio__item--list1'>
                         <li>Secure Bcrypt login</li>
                         <li>Fast character creation</li>
                         <li>generated stats from DND API </li>
