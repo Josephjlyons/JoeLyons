75932e4e116ca096e474e3eaf0cf7e21cee9145b
josephjlyons <josephjlyons90@gmail.com>
2023-05-04 14:28:35 -0400

    add scroll animation | icon pulse | item size change

--- src/Components/Portfolio/Portfolio.js
+++ src/Components/Portfolio/Portfolio.js
@@ -1,10 +1,11 @@
 import '../../sass/_portfolio.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faGithub} from '@fortawesome/free-brands-svg-icons';
-import {faGlobe} from '@fortawesome/free-solid-svg-icons';
-import yelpCampMain from '../../img/Screenshot 2022-11-22 at 12-57-42 YelpCamp.png';
-import draughtPickerMain from '../../img/Screenshot 2022-11-22 at 12-48-01 The Draught Picker.png';
-import dndCreator from '../../img/dnd-homepage-Img.webp.png';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faGlobe } from '@fortawesome/free-solid-svg-icons';
+import yelpCampMain from '../../img/yelpCamp.webp';
+import draughtPickerMain from '../../img/draught-picker.webp';
+import dndCreatorMain from '../../img/dnd.webp';
+import { FadeInDown } from '../../FadeInSections';
 
 
 const Portfolio = () => {
@@ -13,51 +14,56 @@ const Portfolio = () => {
         <div id='portfolio' className='portfolio'>
 
             <figure className='portfolio__item portfolio__item--1'>
-                {/* <p className='portfolio__header'>YelpCamp</p> */}
+                <p className='portfolio__header portfolio__header--yelp'>YelpCamp</p>
                 <img className='portfolio__item portfolio__item--yelp' src={yelpCampMain} alt='yelp-camp-main'></img>
                 <div className='portfolio__item--hidden'>
-                <ul className='portfolio__item--list1'>
-                        <li>Secure passport login</li>
-                        <li>Interactive Map</li>
-                        <li>image uploading</li>
-                        <li>Protected routes</li>
-                        <li>Easy to use interface</li>
-                    </ul>
+                    <FadeInDown>
+                        <ul className='portfolio__item--list1'>
+                            <li>Secure passport login</li>
+                            <li>Interactive Map</li>
+                            <li>image uploading</li>
+                            <li>Protected routes</li>
+                            <li>Easy to use interface</li>
+                        </ul>
 
-                    <a href="https://github.com/Josephjlyons/YelpCamp" aria-label='github'><FontAwesomeIcon className="portfolio__icons" icon={faGithub} /></a>
+                        <a href="https://github.com/Josephjlyons/YelpCamp" aria-label='github'><FontAwesomeIcon className="portfolio__icons" icon={faGithub} /></a>
+                    </FadeInDown>
                 </div>
             </figure>
 
             <figure className='portfolio__item portfolio__item--2'>
-                {/* <p className='portfolio__header'>Draught Picker</p> */}
+                <p className='portfolio__header'>Draught Picker</p>
                 <img className='portfolio__item portfolio__item--draught' src={draughtPickerMain} alt='draught-main'></img>
                 <div className='portfolio__item--hidden'>
-                    <ul className='portfolio__item--list2'>
-                        <li>OpenBreweryDB API</li>
-                        <li>Search by zip codes</li>
-                        <li>Favorite and save breweries</li>
-                        <li>View brewery info</li>
-                        <li>Map location of brewery</li>
-                    </ul>
-                    <a href="https://github.com/Josephjlyons/DraughtPicker"aria-label='github'><FontAwesomeIcon className="portfolio__icons" icon={faGithub} /></a>
-                    <a href='https://josephjlyons.github.io/DraughtPicker/'aria-label='website'><FontAwesomeIcon className="portfolio__icons" icon={faGlobe} /></a>
+                    <FadeInDown>
+                        <ul className='portfolio__item--list2'>
+                            <li>OpenBreweryDB API</li>
+                            <li>Search by zip codes</li>
+                            <li>Favorite and save breweries</li>
+                            <li>View brewery info</li>
+                            <li>Map location of brewery</li>
+                        </ul>
+                        <a href="https://github.com/Josephjlyons/DraughtPicker" aria-label='github'><FontAwesomeIcon className="portfolio__icons" icon={faGithub} /></a>
+                        <a href='https://josephjlyons.github.io/DraughtPicker/' aria-label='website'><FontAwesomeIcon className="portfolio__icons" icon={faGlobe} /></a>
+                    </FadeInDown>
                 </div>
 
             </figure>
 
             <figure className='portfolio__item portfolio__item--3'>
-                {/* <p className='portfolio__header'>DnD Creator</p> */}
-                <img className='portfolio__item portfolio__item--dnd' src={dndCreator} alt='dnd-main'></img>
+                <p className='portfolio__header'>DnD Creator</p>
+                <img className='portfolio__item portfolio__item--dnd' src={dndCreatorMain} alt='dnd-main'></img>
                 <div className='portfolio__item--hidden'>
-
-                <ul className='portfolio__item--list1'>
-                        <li>Secure Bcrypt login</li>
-                        <li>Fast character creation</li>
-                        <li>generated from DND API </li>
-                        <li>view multiple characters</li>
-                        <li>Easily swap characters</li>
-                    </ul>
-                    <a href="https://github.com/tjvig94/dnd-exp" aria-label='github'><FontAwesomeIcon className="portfolio__icons" icon={faGithub} /></a>
+                    <FadeInDown>
+                        <ul className='portfolio__item--list1'>
+                            <li>Secure Bcrypt login</li>
+                            <li>Fast character creation</li>
+                            <li>generated from DND API </li>
+                            <li>view multiple characters</li>
+                            <li>Easily swap characters</li>
+                        </ul>
+                        <a href="https://github.com/tjvig94/dnd-exp" aria-label='github'><FontAwesomeIcon className="portfolio__icons" icon={faGithub} /></a>
+                    </FadeInDown>
                 </div>
 
             </figure>
@@ -66,5 +72,5 @@ const Portfolio = () => {
     )
 }
 
-   
+
 export default Portfolio;
