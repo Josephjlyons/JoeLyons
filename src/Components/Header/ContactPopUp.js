f0827b160fd7f4590cb751f095b45946139117e9
josephjlyons <josephjlyons90@gmail.com>
2023-05-16 12:56:25 -0400

    create contact popup

---
+++ src/Components/Header/ContactPopUp.js
@@ -0,0 +1,34 @@
+import React from "react";
+import '../../sass/_popUp.scss';
+import photo from '../../img/photoMe2.png';
+
+const PopUp = ({ open, onClose }) => {
+if (!open) return null;
+return(
+    <div onClick={onClose} className='overlay'>
+    <div
+      onClick={(e) => {
+        e.stopPropagation();
+      }}
+      className='modalContainer'
+    >
+
+      <div className='popUp'>
+        <p className='popUp__closeBtn' onClick={onClose}>
+          X
+        </p>
+        <div className='popUp__content'>
+          <p></p>
+          <h1>Contact Me</h1>
+          <img className="popUp__content--img" src={photo} alt="joseph lyons" />
+          <p>Email: josephjlyons90@gmail.com</p>
+          <p>Phone: (603) 707-1045</p>
+        </div>
+
+        </div>
+      </div>
+    </div>
+
+);
+}
+export default PopUp;
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,18 +1,23 @@
+import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { faAt, faFile } from '@fortawesome/free-solid-svg-icons';
+import PopUp from './ContactPopUp';
 import '../../sass/_header.scss';
 
 
 const Header = () => {
+const [openContact, setOpenContact] = useState(false)
+
     return (
         <div className='header'>
             <ul id="nav" className="header__nav">
 
-                <li><a className="header__smoothscroll" href="#about">About</a></li>
-                <li><a className="header__smoothscroll" href="#portfolio">Projects</a></li>
-                <li><a className="header__smoothscroll" href="#contact">Contact</a></li>
-                <li><a className="header__smoothscroll" href='https://docs.google.com/document/d/14y2lHTV8S7WNrUancx8AV5NL0C6BFgX0oCL4gwMoIgk/export?format=pdf' aria-label='resume'>Resume</a></li>
+                <li><a className="header__nav--item" href="#about">About</a></li>
+                <li><a className="header__nav--item" href="#portfolio">Projects</a></li>
+                <li><a className="header__nav--item" onClick={() => setOpenContact(true)} href="#contact">Contact</a></li>
+                <PopUp open={openContact} onClose={() => setOpenContact(false)}/>
+                <li><a className="header__nav--item" href='https://docs.google.com/document/d/14y2lHTV8S7WNrUancx8AV5NL0C6BFgX0oCL4gwMoIgk/export?format=pdf' aria-label='resume'>Resume</a></li>
             </ul>
             <h1 className='header__h1'>Joe Lyons</h1>
             <p className='header__text'>full stack developer specializing in front end engineering</p>
