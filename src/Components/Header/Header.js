d1f36994133f095b33f5b8581501c55bca516a37
josephjlyons <josephjlyons90@gmail.com>
2023-05-18 10:56:52 -0400

    add named download for resume link download

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -17,7 +17,7 @@ const [openContact, setOpenContact] = useState(false)
                 <li><a className="header__nav--item" href="#portfolio">Projects</a></li>
                 <li><a className="header__nav--item" onClick={() => setOpenContact(true)} href="#contact">Contact</a></li>
                 <PopUp open={openContact} onClose={() => setOpenContact(false)}/>
-                <li><a className="header__nav--item" href='https://docs.google.com/document/d/14y2lHTV8S7WNrUancx8AV5NL0C6BFgX0oCL4gwMoIgk/export?format=pdf' aria-label='resume'>Resume</a></li>
+                <li><a className="header__nav--item" href='https://docs.google.com/document/d/14y2lHTV8S7WNrUancx8AV5NL0C6BFgX0oCL4gwMoIgk/export?format=pdf' download='Joseph Lyons Resume' aria-label='resume'>Resume</a></li>
             </ul>
             <h1 className='header__h1'>Joe Lyons</h1>
             <p className='header__text'>full stack developer specializing in front end engineering</p>
